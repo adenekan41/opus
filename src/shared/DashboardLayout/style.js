5c34d4ed93ea9e9d0f5d077b4699b9d6849b0c06
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2019-02-14 13:16:41 +0100

    minor refactor

--- src/shared/DashboardLayout/style.js
+++ src/shared/DashboardLayout/style.js
@@ -14,29 +14,10 @@ export const StyledDashboardLayout = styled.div`
     transition: all 0.3s;
     height: 100vh;
     overflow: auto;
+    background-color: #f5f6fa;
 
     &.Sidebar--active {
       transform: translateX(264px);
-    }
-  }
-
-  .CustomerTable,
-  .ResumeTable {
-    flex: 1;
-    margin-top: 64px;
-  }
-
-  .Drawer {
-    box-shadow: -25px 1px 25px 0 rgba(0, 0, 0, 0.02),
-      2px 0 11px 0 rgba(0, 0, 0, 0.25);
-  }
-
-  .CustomerViewDrawer {
-    z-index: 1;
-
-    .Drawer {
-      top: 64px;
-      transition: width 0.3s;
     }
   }
 
