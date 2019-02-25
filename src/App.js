a0bc3d46664d75f11d267145c28e8123ee2968b3
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2019-02-25 08:27:13 +0100

    change router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './App.css';
 import PrimaryLayout from './shared/Layout/primaryLayout';
 import { loadState, saveState, clearState } from './localStorage';
