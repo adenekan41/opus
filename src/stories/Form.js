82143727a41190a9b8229e2d65c4fe1b01fae9b1
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2019-02-26 10:39:37 +0100

    add select component

--- src/stories/Form.js
+++ src/stories/Form.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import SearchInput from '../components/SearchInput';
 import Input from '../components/Input';
+import Dropdown from '../components/Select';
 
 const stories = storiesOf('Form elements', module)
 
@@ -11,3 +12,5 @@ stories.add("Input with icon", () => <Input icon="user"/>)
 stories.add("Input with strip", () => <Input icon="user" hasStrip/>)
 stories.add("Invalid Input", () => <Input isInvalid icon="lock"/>)
 stories.add("Disabled Input", () => <Input disabled icon="lock"/>)
+stories.add("Select", () => <Dropdown />)
+stories.add("Disabled Select", () => <Dropdown isDisabled />)
